02a31ab88cd679b956484f43722dff42980e020b
Sayandeep Patra <144211343+Sayandeep07@users.noreply.github.com>
2025-04-10 08:53:38 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai 
+import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import google.generativeai as genai
